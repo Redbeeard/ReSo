9877427d03e29d4ea36343e6296ebdd08e4b07a3
Sarkasronie <steamy@gmx.net>
2017-03-21 18:02:12 +0100

    initial commit

---
+++ src/main/java/Library.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.Test;
+import main.java.Library;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
